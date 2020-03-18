7370220884612d8807ca16b85038118b37f0b50b
campionfellin <campionfellin@gmail.com>
2020-03-17 21:47:18 -0700

    cdk8s 0.16.0 new formula
    Signed-off-by: campionfellin <campionfellin@gmail.com>

--- Formula/cdk8s.rb
+++ Formula/cdk8s.rb
@@ -3,21 +3,18 @@ require "language/node"
 class Cdk8s < Formula
   desc "Define Kubernetes native apps using object-oriented programming"
   homepage "https://github.com/awslabs/cdk8s"
-  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-0.15.1.tgz"
-  sha256 "31b1f47b3d6c74ed50c029d16765d0f7b22ba513927f1987ebbc7e5eec401465"
+  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-0.16.0.tgz"
+  sha256 "fb4a79c1b009d369d7f125404dc760eeab8af82163ecea64785c3ea0206104d8"
 
   depends_on "node"
 
   def install
-    rm_rf "bin/cdk8s"
-
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
-    shell_output("#{bin}/cdk8s import k8s -l python")
-
+    system "#{bin}/cdk8s", "import", "k8s", "-l", "python"
     assert_predicate testpath/"imports/k8s", :exist?, "cdk8s import did not work"
   end
 end
